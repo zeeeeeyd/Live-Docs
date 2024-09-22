316370ba90696d7a9be1357a31da6d47c3bb3bab
Zeyd SAIAD <zeyd.saiad@etu.univ-mosta.dz>
2024-09-22 23:31:19 +0100

    initial commit with config

---
+++ live-docs/app/layout.tsx
@@ -0,0 +1,40 @@
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import './globals.css'
+import { Metadata } from "next"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: 'LiveDocs',
+  description: 'Your go-to collaborative editor',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    // <ClerkProvider
+    //   appearance={{
+    //     baseTheme: dark,
+    //     variables: { 
+    //       colorPrimary: "#3371FF" ,
+    //       fontSize: '16px'
+    //     },
+    //   }}
+    // >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    // </ClerkProvider>
+  )
+}
